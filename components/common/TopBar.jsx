a2664ad08f25db6309ac5ef4a1974fcf61b1f36c
Ngoan Tran <ngoan98travinh@gmail.com>
2021-05-24 21:02:01 +0700

    add image example

--- components/common/TopBar.jsx
+++ components/common/TopBar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from "./TopBar.module.css";
+import Image from "next/image";
 
 function TopBar() {
-  return <div className={styles.text}>Top bar</div>;
+  return (
+    <div className={styles.text}>
+      <Image src="/images/vercel.svg" alt="example" width={300} height={100} />
+      <h1>Top bar</h1>
+      <p>Hello </p>
+    </div>
+  );
 }
 
 export default TopBar;
