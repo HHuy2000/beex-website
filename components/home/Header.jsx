e0fb03ac14c664296ad315ea22275cc433d1ece5
RUKICUA <bxv.se.gd@gmail.com>
2021-05-24 22:34:02 +0700

    Create header module

--- components/home/Header.jsx
+++ components/home/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import styles from "./Header.module.css";
 
 function Header() {
     return (
         <div>
-            
+            <p>CÙNG BEEX ĐƯA BẠN ĐẾN CÂU CHUYỆN</p>
+            <h1>Những chú ong chăm chỉ</h1>
+            <h1>Cùng bạn xây thương hiệu</h1>
         </div>
     )
 }
