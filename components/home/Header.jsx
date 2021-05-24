c604de035209a5aa84185d00de3b28bd40449047
RUKICUA <bxv.se.gd@gmail.com>
2021-05-24 23:06:09 +0700

    Update basic header items

--- components/home/Header.jsx
+++ components/home/Header.jsx
@@ -3,10 +3,17 @@ import styles from "./Header.module.css";
 
 function Header() {
     return (
-        <div>
+        <div className={styles.div}>
+            <div className={styles.text}>
             <p>CÙNG BEEX ĐƯA BẠN ĐẾN CÂU CHUYỆN</p>
-            <h1>Những chú ong chăm chỉ</h1>
-            <h1>Cùng bạn xây thương hiệu</h1>
+            <h1 className={styles.h1}>Những chú ong chăm chỉ</h1>
+            <h1 className={styles.h1}>Cùng bạn xây thương hiệu</h1>
+            <p className={styles.quotelow}>BeeX là nơi tuyệt vời để bắt đầu hiện thực hoá ý tưởng kinh doanh của bạn. Với những chiến dịch giúp gia tăng doanh số bán hàng từ đó hướng đến sự thành công của doanh nghiệp bạn</p>
+            <button>Liên hệ</button>
+            </div>
+            <div className={styles.pic}>
+            <img src="\images\Ded Inside.png" alt="Dead" className={styles.png} />
+            </div>
         </div>
     )
 }
