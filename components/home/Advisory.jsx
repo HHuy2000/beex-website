7476a2e3226df98489715d7f28cff01a6a7a81d9
Muc Muc <tranhuutri0909@gmail.com>
2021-05-26 20:17:55 +0700

    đang làm tới TextArea basic

--- components/home/Advisory.jsx
+++ components/home/Advisory.jsx
@@ -16,9 +16,27 @@ function Advisory() {
                         btn - hotline*/}
                 <div>  
                     <h1>Yêu cầu tư vấn</h1>
+
+                    <div className = {styles.AdvisoryContent}>
                     <p>BeeX liên tục hoàn thiện dịch vụ khách hàng.</p>
                     <p>Chúng tôi muốn bạn cảm nhận được thành ý từ đội ngũ.</p>
                     <p>Vì thế, chúng tôi sẵn sàng tư vấn miễn phí nếu bạn có bất kì thắc mắc về dịch vụ mà chúng tôi cung cấp.</p>
+                    </div>
+        
+                    <div className = {styles.AdvisoryTextField}>
+                        <div>item 1</div>
+                        <div>item 2</div>
+                        <div>item 3</div>
+                        <div>item 4</div>
+                    </div>
+
+                    <div className = {styles.AdvisoryTextArea}>
+                        <div>Mô tả yêu cầu</div>
+                    </div>
+
+                    <div className = {styles.AdvisoryFooter}>
+
+                    </div>
                 </div>
 
                 <div>
