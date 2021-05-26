80cc7722e2ffd23977c62403c25a5673f7453b1a
Muc Muc <tranhuutri0909@gmail.com>
2021-05-26 20:57:55 +0700

    HTML-basic build for ADV

--- components/home/Advisory.jsx
+++ components/home/Advisory.jsx
@@ -23,19 +23,37 @@ function Advisory() {
                     <p>Vì thế, chúng tôi sẵn sàng tư vấn miễn phí nếu bạn có bất kì thắc mắc về dịch vụ mà chúng tôi cung cấp.</p>
                     </div>
         
-                    <div className = {styles.AdvisoryTextField}>
-                        <div>item 1</div>
-                        <div>item 2</div>
-                        <div>item 3</div>
-                        <div>item 4</div>
+                    <div className = {styles.AdvisoryTextFieldGroup}>
+                        <div className = {styles.AdvisoryTextField}>
+                            <p>Tên của bạn*</p>
+                            <input type="text" id="firstname" name="firstname"/><br/>
+                        </div>
+                        <div className = {styles.AdvisoryTextField}>
+                            <p>Số điện thoại*</p>
+                            <input type="text" id="firstname" name="firstname"/><br/>
+                        </div>
+                        <div className = {styles.AdvisoryTextField}>
+                            <p>Email*</p>
+                            <input type="text" id="firstname" name="firstname"/><br/>
+                        </div>
+                        <div className = {styles.AdvisoryTextField}>
+                            <p>Công ty</p>
+                            <input type="text" id="firstname" name="firstname"/><br/>
+                        </div>
                     </div>
 
                     <div className = {styles.AdvisoryTextArea}>
                         <div>Mô tả yêu cầu</div>
+                        <textarea rows="9" cols="70"></textarea>  
                     </div>
 
                     <div className = {styles.AdvisoryFooter}>
-
+                        <div>
+                            <button name="button" type="button" className = {styles.AdvisoryButton}>Gửi yêu cầu</button>
+                        </div>
+                        <div >
+                            Hotline: 0327846162
+                        </div>
                     </div>
                 </div>
 
