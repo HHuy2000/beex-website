e6b9c7d19fc004eaeb8995a11f55d9c4a5b554bc
PQHV1999 <qhvu.phan@gmail.com>
2021-05-26 20:03:34 +0700

    dev/services success 90%

--- components/home/Services.jsx
+++ components/home/Services.jsx
@@ -3,13 +3,78 @@ import ServiceItem from "./ServiceItem";
 
 function Services() {
   return (
-    <div>
-      Services
-      <ServiceItem title="Tieu de A" items={["123", "2354234"]} />
-      <ServiceItem title="Tieu de B" />
-      <ServiceItem title="Tieu de C" />
-      <ServiceItem title="Tieu de D" />
-    </div>
+    <>
+        <div class="div-title">
+        <h1 class="title">Dịch vụ</h1>
+        <p class="img-bg"></p>
+        </div>
+      <section class="section-main section-one">
+          <div class="div-one">
+              <h4 class="header">Thiết kế nhận diện thương hiệu</h4>
+              <ul>
+                  <li class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Tư vấn thương hiệu</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i> Sáng tạo nội dung</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Thiết kế thương hiệu</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Thiết kế bao bì</li>
+              </ul>
+              <button class="div-one-button">Liên hệ</button>
+          </div>
+          <div class="div-two">
+              <img class="img-div-two" src="../images/services-img1.svg" alt="" />
+          </div>
+      </section>
+      <section id="section-two" class="section-main section-two">
+          <div class="div-one">
+              <h4 class="header">Phát triển phần mềm</h4>
+              <ul>
+                  <li class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Xây dựng landing sáng tạo</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i> Xây dựng website điện tử</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Xây dựng phần mềm quản lí doanh nghiệp CRM, LMS, CMS,...</li>                
+              </ul>
+              <button class="div-one-button">Liên hệ</button>
+          </div>
+          <div class="div-two">
+              <img class="img-div-two" src="../images/services-img2.svg" alt="" />
+          </div>
+      </section>
+      <section class="section-main section-one">
+          <div class="div-one">
+              <h4 class="header">Phát triển và quản lí Fanpage</h4>
+              <ul>
+                  <li class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Thiết lập, tối ưu hóa fanpage</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Sáng tạo nội dung</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Event / Minigame</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Thiết kế poster</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Thu hút lượt tiếp cận, chạy quảng cáo</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Xây dựng chiến lược quản trị và phát triển fanpage</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Thiết lập Chatbot trả lời tin nhắn tự động</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Trả lời inbox, tư vấn sản phẩm</li>
+              </ul>
+              <button class="div-one-button">Liên hệ</button>
+          </div>
+          <div class="div-two">
+              <img class="img-div-two" src="../images/services-img3.svg" alt="" />
+          </div>
+      </section>
+      <section id="section-two" class="section-main section-two">
+          <div class="div-one">
+              <h4 class="header">Dịch vụ khác</h4>
+              <ul>
+                  <li class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Thiết lập doanh nghiệp trên Google Map</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Hỗ trợ đăng ký tên miền</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Dựng, chỉnh sửa video</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Chỉnh sửa ảnh, blend màu, hậu kì,..</li>
+                  <li  class="div-one-li"><i><img class="img-vector"src="../images/Vector.svg" alt="" /></i>Chụp ảnh sản phẩm</li>
+              </ul>
+              <button class="div-one-button">Liên hệ</button>
+          </div>
+          <div class="div-two">
+              <img class="img-div-two" src="../images/services-img4.svg" alt="" />
+          </div>
+      </section>
+      
+      </>
+
   );
 }
 
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/services.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
