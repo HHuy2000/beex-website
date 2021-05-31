d2f2d752d862356a36f6593b760622fa474a24c6
william-huynh <hnbaolam@gmail.com>
2021-05-31 19:34:27 +0700

    firstCommit

--- components/home/OurProducts.jsx
+++ components/home/OurProducts.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function OurProducts() {
     return (
         <div>
-            Our products
+            Our products  
+            {/*alo*/}
         </div>
     )
 }
