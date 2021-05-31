a76fdf2e38299ce1d5e1f89045bb9fa8c2b34e1e
william-huynh <hnbaolam@gmail.com>
2021-05-31 20:05:38 +0700

    fix-serveral-thing

--- components/home/OurProducts.jsx
+++ components/home/OurProducts.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 function OurProducts() {
     return (
-        <div>
-            Our products  
-            {/*alo*/}
+
+        /*container*/
+        <div class="container">
+
+            {/*header*/}
+            <div class="header">
+                <img src="/images/ourproduct-img1"/>
+                <p>Sản phẩm của chúng tôi</p>
+            </div>
+
+            {/*frame*/}
+            <div class="frame">
+                <image src=""></image>
+            </div>
         </div>
     )
 }
