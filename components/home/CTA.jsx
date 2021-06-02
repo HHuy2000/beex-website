a46762bdd7392666df43a9d9ab71ba211f51cbe9
RUKICUA <bxv.se.gd@gmail.com>
2021-06-02 09:05:09 +0700

    input basic text and buttons

--- components/home/CTA.jsx
+++ components/home/CTA.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import styles from './CTA.module.css'
 
 function CTA() {
     return (
         <div>
-            Bạn đã sẵn sàng ?
+            <h1>Bạn đã sẵn sàng?</h1>
+            <p>Trãi nghiệm ngay tất cả các dịch vụ tối ưu và tiết kiệm nhất cho doanh nghiệp của bạn được cung cấp từ BeeX chúng tôi</p>
+            <button>Liên hệ ngay</button>
+            <button>Xem bảng chi tiết nhiệm vụ</button>
         </div>
     )
 }
