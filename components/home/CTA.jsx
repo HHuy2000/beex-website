22557c49e7b978eed95cf8b684224a456ee473a5
RUKICUA <bxv.se.gd@gmail.com>
2021-06-02 09:50:24 +0700

    edit to match design

--- components/home/CTA.jsx
+++ components/home/CTA.jsx
@@ -7,7 +7,7 @@ function CTA() {
             <img src="/images/CTAicon.svg" alt="CTA icon" className={styles.CTAIcon}/>
             <div className={styles.CTA}>
                 <h1 className={styles.CTAH1}>Bạn đã sẵn sàng?</h1>
-                <p>Trãi nghiệm ngay tất cả các dịch vụ tối ưu và tiết kiệm nhất cho doanh nghiệp của bạn được cung cấp từ BeeX chúng tôi</p>
+                <p className={styles.CTAP}>Trãi nghiệm ngay tất cả các dịch vụ tối ưu và tiết kiệm nhất cho doanh nghiệp của bạn được cung cấp từ BeeX chúng tôi</p>
                 <div className={styles.CTAButtons}>
                 <button className={styles.CTAContactButton}>Liên hệ ngay</button>
                 <button className={styles.CTADetailButton}>Xem bảng chi tiết nhiệm vụ</button>
