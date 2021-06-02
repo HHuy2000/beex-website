f3e2696769bd7da805282a45210bc1ffe61e83e3
william-huynh <hnbaolam@gmail.com>
2021-06-02 20:11:13 +0700

    finish html

--- components/home/OurProducts.jsx
+++ components/home/OurProducts.jsx
@@ -19,8 +19,8 @@ function OurProducts() {
                 <img class="vector" id="vector1" src="/images/ourproduct-vector1.svg"/>
                 <img class="vector" id="vector2" src="/images/ourproduct-vector2.svg"/>
 
-                {/*container-grid*/}
-                <div class="container-grid">
+                {/*container-box*/}
+                <div class="container-box">
 
                     {/*grid-content*/}
                     <div class="grid-content">
@@ -31,8 +31,8 @@ function OurProducts() {
                             {/*grid-content-container*/}
                             <div class="grid-content-container">
 
-                                {/*grid-content-upper*/}
-                                <div class="grid-content-upper">
+                                {/*grid-content-container-upper*/}
+                                <div class="grid-content-container-upper">
                                     <img src="/images/ourproduct-jamstack.svg"/>
                                     <div class="profile-name">
                                         <h3>JAMstack Vietnam</h3>
@@ -40,8 +40,8 @@ function OurProducts() {
                                     </div>
                                 </div>
 
-                                {/*grid-content-lower*/}
-                                <div class="grid-content-lower">
+                                {/*grid-content-container-lower*/}
+                                <div class="grid-content-container-lower">
                                     <div class="status">
                                         <img src="/images/ourproduct-like.svg"/>
                                         <h3>132.554 people like this</h3>
@@ -63,9 +63,115 @@ function OurProducts() {
                         {/*grid-content-lower*/}
                         <div class="grid-content-lower">
 
+                            {/*grid-content-image*/}                            
+                            <img src="/images/ourproduct-mstore.svg" class="grid-content-image"/>                            
+
+                            {/*grid-content-comment*/}
+                            <div class="grid-content-comment">
+                                <h4>Landing page MStore</h4>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    {/*grid-content*/}
+                    <div class="grid-content">
+
+                        {/*grid-content-upper*/}
+                        <div class="grid-content-upper">
+                        
                             {/*grid-content-container*/}
-                            <div class="grid-content-image">
-                                <img src="/images/ourproduct-mstore.svg"/>
+                            <div class="grid-content-container">
+
+                                {/*grid-content-container-upper*/}
+                                <div class="grid-content-container-upper">
+                                    <img src="/images/ourproduct-chon.svg"/>
+                                    <div class="profile-name">
+                                        <h3>Chồn Lùi Bích Chy</h3>
+                                        <h4>@chonluibichchy</h4>
+                                    </div>
+                                </div>
+
+                                {/*grid-content-container-lower*/}
+                                <div class="grid-content-container-lower">
+                                    <div class="status">
+                                        <img src="/images/ourproduct-like.svg"/>
+                                        <h3>33.524 people like this</h3>
+                                    </div>
+                                    <div class="status">
+                                        <img src="/images/ourproduct-checkmark.svg"/>
+                                        <h3>34.594 people follow this</h3>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/*grid-content-comment*/}
+                            <div class="grid-content-comment">
+                                <h4>Fanpage Facebook</h4>
+                            </div>
+
+                        </div>
+
+                        <div class="grid-content-lower">
+
+                            {/*grid-content-image*/}
+                            <img src="/images/ourproduct-journal.svg" class="grid-content-image"/>                            
+
+                            {/*grid-content-comment*/}
+                            <div class="grid-content-comment">
+                                <h4>Landing page Journal</h4>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    {/*grid-content*/}
+                    <div class="grid-content">
+
+                        {/*grid-content-upper*/}
+                        <div class="grid-content-upper">
+                        
+                            {/*grid-content-container*/}
+                            <div class="grid-content-container">
+
+                                {/*grid-content-container-upper*/}
+                                <div class="grid-content-container-upper">
+                                    <img src="/images/ourproduct-sharefast.svg"/>
+                                    <div class="profile-name">
+                                        <h3>sharefast.me</h3>
+                                        <h4>@sharefast.me</h4>
+                                    </div>
+                                </div>
+
+                                {/*grid-content-container-lower*/}
+                                <div class="grid-content-container-lower">
+                                    <div class="status">
+                                        <img src="/images/ourproduct-like.svg"/>
+                                        <h3>2.437 people like this</h3>
+                                    </div>
+                                    <div class="status">
+                                        <img src="/images/ourproduct-checkmark.svg"/>
+                                        <h3>2.137 people follow this</h3>
+                                    </div>
+                                </div>
+                            
+                                {/*grid-content-comment*/}
+                                <div class="grid-content-comment">
+                                    <h4>Fanpage Facebook</h4>
+                                </div>
+
+                            </div>
+
+                            <div class="grid-content-lower">
+
+                                {/*grid-content-image*/}
+                                <img src="/images/ourproduct-flextop.svg" class="grid-content-image"/>                            
+
+                                {/*grid-content-comment*/}
+                                <div class="grid-content-comment">
+                                    <h4>Landing page Flextop</h4>
+                                </div>
+
                             </div>
                         </div>
                     </div>
@@ -73,102 +179,51 @@ function OurProducts() {
                     {/*grid-content*/}
                     <div class="grid-content">
 
-                        {/*grid-content-container*/}
-                        <div class="grid-content-container">
+                        {/*grid-content-upper*/}
+                        <div class="grid-content-upper">
 
-                            {/*grid-content-upper*/}
-                            <div class="grid-content-upper">
-                                <img src="/images/ourproduct-chon.svg"/>
-                                <div class="profile-name">
-                                    <h3>Chồn Lùi Bích Chy</h3>
-                                    <h4>@chonluibichchy</h4>
+                            {/*grid-content-container*/}
+                            <div class="grid-content-container">
+
+                                {/*grid-content-upper*/}
+                                <div class="grid-content-container-upper">
+                                    <img src="/images/ourproduct-mapas.svg"/>
+                                    <div class="profile-name">
+                                        <h3>MAPAS</h3>
+                                        <h4>@mapasdesign</h4>
+                                    </div>
+                                </div>
+
+                                {/*grid-content-lower*/}
+                                <div class="grid-content-container-lower">
+                                    <div class="status">
+                                        <img src="/images/ourproduct-like.svg"/>
+                                        <h3>62.437 people like this</h3>
+                                    </div>
+                                    <div class="status">
+                                        <img src="/images/ourproduct-checkmark.svg"/>
+                                        <h3>67.137 people follow this</h3>
+                                    </div>
                                 </div>
                             </div>
 
-                            {/*grid-content-lower*/}
-                            <div class="grid-content-lower">
-                                <div class="status">
-                                    <img src="/images/ourproduct-like.svg"/>
-                                    <h3>33.524 people like this</h3>
-                                </div>
-                                <div class="status">
-                                    <img src="/images/ourproduct-checkmark.svg"/>
-                                    <h3>34.594 people follow this</h3>
-                                </div>
+                            {/*grid-content-comment*/}
+                            <div class="grid-content-comment">
+                                <h4>Fanpage Facebook</h4>
                             </div>
+                        
                         </div>
 
-                        {/*grid-content-comment*/}
-                        <div class="grid-content-comment">
-                            <h4>Fanpage Facebook</h4>
-                        </div>
-                    </div>
+                        <div class="grid-content-lower">
 
-                    {/*grid-content*/}
-                    <div class="grid-content">
+                            {/*grid-content-image*/}
+                            <img src="/images/ourproduct-limpa.svg" class="grid-content-image"/>                            
 
-                        {/*grid-content-container*/}
-                        <div class="grid-content-container">
-
-                            {/*grid-content-upper*/}
-                            <div class="grid-content-upper">
-                                <img src="/images/ourproduct-sharefast.svg"/>
-                                <div class="profile-name">
-                                    <h3>sharefast.me</h3>
-                                    <h4>@sharefast.me</h4>
-                                </div>
+                            {/*grid-content-comment*/}
+                            <div class="grid-content-comment">
+                                <h4>Web bán hàng Limpa</h4>
                             </div>
 
-                            {/*grid-content-lower*/}
-                            <div class="grid-content-lower">
-                                <div class="status">
-                                    <img src="/images/ourproduct-like.svg"/>
-                                    <h3>2.437 people like this</h3>
-                                </div>
-                                <div class="status">
-                                    <img src="/images/ourproduct-checkmark.svg"/>
-                                    <h3>2.137 people follow this</h3>
-                                </div>
-                            </div>
-                        </div>
-
-                        {/*grid-content-comment*/}
-                        <div class="grid-content-comment">
-                            <h4>Fanpage Facebook</h4>
-                        </div>
-                    </div>
-
-                    {/*grid-content*/}
-                    <div class="grid-content">
-
-                        {/*grid-content-container*/}
-                        <div class="grid-content-container">
-
-                            {/*grid-content-upper*/}
-                            <div class="grid-content-upper">
-                                <img src="/images/ourproduct-mapas.svg"/>
-                                <div class="profile-name">
-                                    <h3>MAPAS</h3>
-                                    <h4>@mapasdesign</h4>
-                                </div>
-                            </div>
-
-                            {/*grid-content-lower*/}
-                            <div class="grid-content-lower">
-                                <div class="status">
-                                    <img src="/images/ourproduct-like.svg"/>
-                                    <h3>62.437 people like this</h3>
-                                </div>
-                                <div class="status">
-                                    <img src="/images/ourproduct-checkmark.svg"/>
-                                    <h3>67.137 people follow this</h3>
-                                </div>
-                            </div>
-                        </div>
-
-                        {/*grid-content-comment*/}
-                        <div class="grid-content-comment">
-                            <h4>Fanpage Facebook</h4>
                         </div>
                     </div>
 
