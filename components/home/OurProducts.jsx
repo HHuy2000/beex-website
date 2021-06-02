86c0781715e30a5cce1f213bec0ac68c74abca1f
william-huynh <hnbaolam@gmail.com>
2021-06-02 21:28:01 +0700

    css

--- components/home/OurProducts.jsx
+++ components/home/OurProducts.jsx
@@ -1,174 +1,175 @@
 import React from 'react'
+import hello from './OurProducts.module.css'
 
 function OurProducts() {
     return (
 
-        /*container*/
-        <div class="container">
+        /*anchor background*/
+        <div className={hello.anchor}>
+            <img className={hello.header_img} src="/images/ourproduct-img1.svg"/>
+
+        {/*container*/}
+        <div className={hello.container}>
 
             {/*header*/}
-            <div class="header">
-                <img id="header-img" src="/images/ourproduct-img1.svg"/>
-                <p>Sản phẩm của chúng tôi</p>
+            <div className={hello.header}>
+                
+                <h1>Sản phẩm của chúng tôi</h1>
             </div>
 
             {/*frame*/}
-            <div class="frame">
+            <div className={hello.frame}>
+            
+                {/*container_box*/}
+                <div className={hello.container_box}>
 
-                {/*vector*/}
-                <img class="vector" id="vector1" src="/images/ourproduct-vector1.svg"/>
-                <img class="vector" id="vector2" src="/images/ourproduct-vector2.svg"/>
+                    {/*grid_content*/}
+                    <div className={hello.grid_content}>
 
-                {/*container-box*/}
-                <div class="container-box">
+                        {/*grid_content_upper*/}
+                        <div className={hello.grid_content_upper}>
 
-                    {/*grid-content*/}
-                    <div class="grid-content">
+                            {/*grid_content_container*/}
+                            <div className={hello.grid_content_container}>
 
-                        {/*grid-content-upper*/}
-                        <div class="grid-content-upper">
-
-                            {/*grid-content-container*/}
-                            <div class="grid-content-container">
-
-                                {/*grid-content-container-upper*/}
-                                <div class="grid-content-container-upper">
+                                {/*grid_content_container_upper*/}
+                                <div className={hello.grid_content_container_upper}>
                                     <img src="/images/ourproduct-jamstack.svg"/>
-                                    <div class="profile-name">
+                                    <div className={hello.profile_name}>
                                         <h3>JAMstack Vietnam</h3>
                                         <h4>@JAMstackVietnam</h4>
                                     </div>
                                 </div>
 
-                                {/*grid-content-container-lower*/}
-                                <div class="grid-content-container-lower">
-                                    <div class="status">
+                                {/*grid_content_container_lower*/}
+                                <div className={hello.grid_content_container_lower}>
+                                    <div className={hello.status}>
                                         <img src="/images/ourproduct-like.svg"/>
                                         <h3>132.554 people like this</h3>
                                     </div>
-                                    <div class="status">
+                                    <div className={hello.status}>
                                         <img src="/images/ourproduct-checkmark.svg"/>
                                         <h3>233.247 people follow this</h3>
                                     </div>
                                 </div>
                             </div>
                         
-                            {/*grid-content-comment*/}
-                            <div class="grid-content-comment">
+                            {/*grid_content_comment*/}
+                            <div className={hello.grid_content_comment}>
                                 <h4>Fanpage Facebook</h4>
                             </div>
                             
                         </div>
 
-                        {/*grid-content-lower*/}
-                        <div class="grid-content-lower">
+                        {/*grid_content_lower*/}
+                        <div className={hello.grid_content_lower}>
 
-                            {/*grid-content-image*/}                            
-                            <img src="/images/ourproduct-mstore.svg" class="grid-content-image"/>                            
+                            {/*grid_content_image*/}                            
+                            <img src="/images/ourproduct-mstore.svg" className={hello.grid_content_image}/>                            
 
-                            {/*grid-content-comment*/}
-                            <div class="grid-content-comment">
+                            {/*grid_content_comment*/}
+                            <div className={hello.grid_content_comment}>
                                 <h4>Landing page MStore</h4>
                             </div>
 
                         </div>
                     </div>
 
-                    {/*grid-content*/}
-                    <div class="grid-content">
+                    {/*grid_content*/}
+                    <div className={hello.grid_content}>
 
-                        {/*grid-content-upper*/}
-                        <div class="grid-content-upper">
+                        {/*grid_content_upper*/}
+                        <div className={hello.grid_content_upper}>
                         
-                            {/*grid-content-container*/}
-                            <div class="grid-content-container">
+                            {/*grid_content_container*/}
+                            <div className={hello.grid_content_container}>
 
-                                {/*grid-content-container-upper*/}
-                                <div class="grid-content-container-upper">
+                                {/*grid_content_container_upper*/}
+                                <div className={hello.grid_content_container_upper}>
                                     <img src="/images/ourproduct-chon.svg"/>
-                                    <div class="profile-name">
+                                    <div className={hello.profile_name}>
                                         <h3>Chồn Lùi Bích Chy</h3>
                                         <h4>@chonluibichchy</h4>
                                     </div>
                                 </div>
 
-                                {/*grid-content-container-lower*/}
-                                <div class="grid-content-container-lower">
-                                    <div class="status">
+                                {/*grid_content_container_lower*/}
+                                <div className={hello.grid_content_container_lower}>
+                                    <div className={hello.status}>
                                         <img src="/images/ourproduct-like.svg"/>
                                         <h3>33.524 people like this</h3>
                                     </div>
-                                    <div class="status">
+                                    <div className={hello.status}>
                                         <img src="/images/ourproduct-checkmark.svg"/>
                                         <h3>34.594 people follow this</h3>
                                     </div>
                                 </div>
                             </div>
 
-                            {/*grid-content-comment*/}
-                            <div class="grid-content-comment">
+                            {/*grid_content_comment*/}
+                            <div className={hello.grid_content_comment}>
                                 <h4>Fanpage Facebook</h4>
                             </div>
 
                         </div>
 
-                        <div class="grid-content-lower">
+                        <div className={hello.grid_content_lower}>
 
-                            {/*grid-content-image*/}
-                            <img src="/images/ourproduct-journal.svg" class="grid-content-image"/>                            
+                            {/*grid_content_image*/}
+                            <img src="/images/ourproduct-journal.svg" className={hello.grid_content_image}/>                            
 
-                            {/*grid-content-comment*/}
-                            <div class="grid-content-comment">
+                            {/*grid_content_comment*/}
+                            <div className={hello.grid_content_comment}>
                                 <h4>Landing page Journal</h4>
                             </div>
 
                         </div>
                     </div>
 
-                    {/*grid-content*/}
-                    <div class="grid-content">
+                    {/*grid_content*/}
+                    <div className={hello.grid_content}>
 
-                        {/*grid-content-upper*/}
-                        <div class="grid-content-upper">
+                        {/*grid_content_upper*/}
+                        <div className={hello.grid_content_upper}>
                         
-                            {/*grid-content-container*/}
-                            <div class="grid-content-container">
+                            {/*grid_content_container*/}
+                            <div className={hello.grid_content_container}>
 
-                                {/*grid-content-container-upper*/}
-                                <div class="grid-content-container-upper">
+                                {/*grid_content_container_upper*/}
+                                <div className={hello.grid_content_container_upper}>
                                     <img src="/images/ourproduct-sharefast.svg"/>
-                                    <div class="profile-name">
+                                    <div className={hello.profile_name}>
                                         <h3>sharefast.me</h3>
                                         <h4>@sharefast.me</h4>
                                     </div>
                                 </div>
 
-                                {/*grid-content-container-lower*/}
-                                <div class="grid-content-container-lower">
-                                    <div class="status">
+                                {/*grid_content_container_lower*/}
+                                <div className={hello.grid_content_container_lower}>
+                                    <div className={hello.status}>
                                         <img src="/images/ourproduct-like.svg"/>
                                         <h3>2.437 people like this</h3>
                                     </div>
-                                    <div class="status">
+                                    <div className={hello.status}>
                                         <img src="/images/ourproduct-checkmark.svg"/>
                                         <h3>2.137 people follow this</h3>
                                     </div>
                                 </div>
                             
-                                {/*grid-content-comment*/}
-                                <div class="grid-content-comment">
+                                {/*grid_content_comment*/}
+                                <div className={hello.grid_content_comment}>
                                     <h4>Fanpage Facebook</h4>
                                 </div>
 
                             </div>
 
-                            <div class="grid-content-lower">
+                            <div className={hello.grid_content_lower}>
 
-                                {/*grid-content-image*/}
-                                <img src="/images/ourproduct-flextop.svg" class="grid-content-image"/>                            
+                                {/*grid_content_image*/}
+                                <img src="/images/ourproduct-flextop.svg" className={hello.grid_content_image}/>                            
 
-                                {/*grid-content-comment*/}
-                                <div class="grid-content-comment">
+                                {/*grid_content_comment*/}
+                                <div className={hello.grid_content_comment}>
                                     <h4>Landing page Flextop</h4>
                                 </div>
 
@@ -176,51 +177,51 @@ function OurProducts() {
                         </div>
                     </div>
 
-                    {/*grid-content*/}
-                    <div class="grid-content">
+                    {/*grid_content*/}
+                    <div className={hello.grid_content}>
 
-                        {/*grid-content-upper*/}
-                        <div class="grid-content-upper">
+                        {/*grid_content_upper*/}
+                        <div className={hello.grid_content_upper}>
 
-                            {/*grid-content-container*/}
-                            <div class="grid-content-container">
+                            {/*grid_content_container*/}
+                            <div className={hello.grid_content_container}>
 
-                                {/*grid-content-upper*/}
-                                <div class="grid-content-container-upper">
+                                {/*grid_content_upper*/}
+                                <div className={hello.grid_content_container_upper}>
                                     <img src="/images/ourproduct-mapas.svg"/>
-                                    <div class="profile-name">
+                                    <div className={hello.profile_name}>
                                         <h3>MAPAS</h3>
                                         <h4>@mapasdesign</h4>
                                     </div>
                                 </div>
 
-                                {/*grid-content-lower*/}
-                                <div class="grid-content-container-lower">
-                                    <div class="status">
+                                {/*grid_content_lower*/}
+                                <div className={hello.grid_content_container_lower}>
+                                    <div className={hello.status}>
                                         <img src="/images/ourproduct-like.svg"/>
                                         <h3>62.437 people like this</h3>
                                     </div>
-                                    <div class="status">
+                                    <div className={hello.status}>
                                         <img src="/images/ourproduct-checkmark.svg"/>
                                         <h3>67.137 people follow this</h3>
                                     </div>
                                 </div>
                             </div>
 
-                            {/*grid-content-comment*/}
-                            <div class="grid-content-comment">
+                            {/*grid_content_comment*/}
+                            <div className={hello.grid_content_comment}>
                                 <h4>Fanpage Facebook</h4>
                             </div>
                         
                         </div>
 
-                        <div class="grid-content-lower">
+                        <div className={hello.grid_content_lower}>
 
-                            {/*grid-content-image*/}
-                            <img src="/images/ourproduct-limpa.svg" class="grid-content-image"/>                            
+                            {/*grid_content_image*/}
+                            <img src="/images/ourproduct-limpa.svg" className={hello.grid_content_image}/>                            
 
-                            {/*grid-content-comment*/}
-                            <div class="grid-content-comment">
+                            {/*grid_content_comment*/}
+                            <div className={hello.grid_content_comment}>
                                 <h4>Web bán hàng Limpa</h4>
                             </div>
 
@@ -228,7 +229,13 @@ function OurProducts() {
                     </div>
 
                 </div>
+
+                {/*vector*/}
+                <img className={hello.move_right} src="/images/ourproduct-vector1.svg"/>
+                <img className={hello.move_left} src="/images/ourproduct-vector2.svg"/>
+
             </div>
+        </div>
         </div>
     )
 }
