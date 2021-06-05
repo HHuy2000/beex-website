8edfd5c98cf154aa62ae527e5d6d1ca86785ea12
RUKICUA <bxv.se.gd@gmail.com>
2021-06-05 19:13:07 +0700

    fixing navbar issue

--- components/common/TopBar.jsx
+++ components/common/TopBar.jsx
@@ -3,13 +3,13 @@ import styles from "./TopBar.module.css";
 import Image from "next/image";
 
 function TopBar() {
-  const [showLinks, setShowLinks] = useState(true);
+  const [showLinks, setShowLinks] = useState(false);
   return (
     <div className={styles.navbar} id={styles.bruh}>
      
       <img src="/images/logo.svg" alt="logo" className={styles.logo}/>
       <div className={styles.navWrapper}>
-        <div className={styles.navContent} id={showLinks ? styles.hidden: ""}>
+        <div className={styles.navContent} id={showLinks ? styles.hidden : ""}>
           <a href=" "><img src="/images/mainpagebee.svg" alt="minibee" /><span className={styles.span}>Trang chủ</span></a>
           <a href="">Dịch vụ</a>
           <a href="">Gói dịch vụ</a>
