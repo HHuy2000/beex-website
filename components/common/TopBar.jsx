edb6b162370d25a405dacebcc4ad22b4ee76e8a8
RUKICUA <bxv.se.gd@gmail.com>
2021-06-07 22:30:41 +0700

    change nav bar animation

--- components/common/TopBar.jsx
+++ components/common/TopBar.jsx
@@ -5,19 +5,21 @@ import Image from "next/image";
 function TopBar() {
   const [showLinks, setShowLinks] = useState(false);
   return (
-    <div className={styles.navbar} id={styles.bruh}>
+    
+    <div className={styles.navbar} >
      
       <img src="/images/logo.svg" alt="logo" className={styles.logo}/>
       <div className={styles.navWrapper}>
-        <div className={styles.navContent} id={showLinks ? styles.hidden : ""}>
+        <div className={styles.navContent} id={showLinks ? styles.menu : ""}>
           <a href=" "><img src="/images/mainpagebee.svg" alt="minibee" /><span className={styles.span}>Trang chủ</span></a>
           <a href="">Dịch vụ</a>
           <a href="">Gói dịch vụ</a>
           <a href="">Về chúng tôi</a>
         </div>
-        <button onClick={()=>setShowLinks(!showLinks)}><img src="/images/mainpagebee.svg" alt="minibee" />MENU</button>
-        <img src="/images/logo.svg" alt="logo" className={styles.logoMobile}/>
+        <button onClick={()=>setShowLinks(!showLinks)}><img src="/images/menuicon.png" alt="menu icon" /></button>
+        
       </div>
+      <img src="/images/logo.svg" alt="logo" className={styles.logoMobile}/>
     </div>
   );
 }
