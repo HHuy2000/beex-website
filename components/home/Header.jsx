16be4978c6ec089303d6e8a6755582fb2542f5bd
RUKICUA <bxv.se.gd@gmail.com>
2021-06-09 10:01:55 +0700

    fixing all problems

--- components/home/Header.jsx
+++ components/home/Header.jsx
@@ -5,9 +5,13 @@ function Header() {
     return (
         <div className={styles.header}>
             <div className={styles.headerTextWrapper}>         
-                 <p className={styles.headerTopP}>CÙNG BEEX ĐƯA BẠN ĐẾN CÂU CHUYỆN</p>
-                <h1 className={styles.headerH1}>Những chú ong <span className={styles.span}>chăm chỉ</span><br />Cùng bạn xây <span className={styles.span}>thương hiệu</span></h1>
-                <p className={styles.headerBottomP}><span className={styles.span}>BeeX</span> là nơi tuyệt vời để bắt đầu hiện thực hoá ý tưởng kinh doanh của bạn. Với những chiến dịch giúp gia tăng doanh số bán hàng từ đó hướng đến sự thành công của doanh nghiệp bạn</p>
+                <p className={styles.headerTopP}>CÙNG BEEX ĐƯA BẠN ĐẾN CÂU CHUYỆN</p>
+                <div className={styles.headerH1Wrapper}>
+                    <h1 className={styles.headerH1}>Những chú ong <span className={styles.span}>chăm chỉ</span><br />Cùng bạn xây <span className={styles.span}>thương hiệu</span></h1>
+                </div>
+                <div className={styles.headerBottomPWrapper}>
+                    <p className={styles.headerBottomP}><span className={styles.span}>BeeX</span> là nơi tuyệt vời để bắt đầu hiện thực hoá ý tưởng kinh doanh của bạn. Với những chiến dịch giúp gia tăng doanh số bán hàng từ đó hướng đến sự thành công của doanh nghiệp bạn</p>
+                </div>
                 <button className={styles.headerButton}>Liên hệ</button>
             </div>
             <div className={styles.headerImage}>
