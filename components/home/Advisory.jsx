d9ed9ed40139fa9889f4880f34eb1cfb379a4b8f
Muc Muc <tranhuutri0909@gmail.com>
2021-06-09 20:23:15 +0700

    xu li nhap du lieu

--- components/home/Advisory.jsx
+++ components/home/Advisory.jsx
@@ -10,26 +10,25 @@ function Advisory() {
                     <div className = {styles.AdvisoryContent}>
                     <p>BeeX liên tục hoàn thiện dịch vụ khách hàng. Chúng tôi muốn bạn cảm nhận được thành ý từ đội ngũ. Vì thế, chúng tôi sẵn sàng tư vấn miễn phí nếu bạn có bất kì thắc mắc về dịch vụ mà chúng tôi cung cấp.</p>
                     </div>
-                    <form>
+                    <form action="https://script.google.com/macros/s/AKfycbwmlEx8bOIK-EK9MuwacguMekZTwYOGmIgHfT9u9wCoYPCd3fl4-areEfOWFQcBPbksQQ/exec" method="post">
                         <div className = {styles.AdvisoryTextFieldGroup}>
                         <div>
                             <p>Tên của bạn*</p>
-                            <input className = {styles.AdvisoryTextField} type="text" id="name" name="name" autoComplete="off"/><br/>
+                            <input className = {styles.AdvisoryTextField} type="text" id="name" autoComplete="off" required/><br/>
                         </div>
                         <div>
                             <p>Số điện thoại*</p>
-                            <input className = {styles.AdvisoryTextField} type="text" id="phoneNumber" name="phoneNumber" autoComplete="off"/><br/>
+                            <input className = {styles.AdvisoryTextField} type="text" id="phoneNumber" autoComplete="off" required/><br/>
                         </div>
                         <div>
                             <p>Email*</p>
-                            <input className = {styles.AdvisoryTextField} type="text" id="email" name="email" autoComplete="off"/><br/>
+                            <input className = {styles.AdvisoryTextField} type="text" id="email" autoComplete="off" required/><br/>
                         </div>
                         <div>
                             <p>Công ty</p>
-                            <input className = {styles.AdvisoryTextField} type="text" id="company" name="company" autoComplete="off"/><br/>
+                            <input className = {styles.AdvisoryTextField} type="text" id="company" autoComplete="off" required/><br/>
                         </div>
                         </div>
-                    </form>
                     <div>
                         <div>
                         <div className = {styles.AdvisoryDes}><br/><br/>Mô tả yêu cầu</div>
@@ -38,13 +37,14 @@ function Advisory() {
 
                         <div className = {styles.AdvisoryFooter}>
                             <div>
-                            <button name="button" type="button" className = {styles.AdvisoryButton}>Gửi yêu cầu</button>
+                            <button name="button" type="submit" className = {styles.AdvisoryButton}>Gửi yêu cầu</button>
                             </div>
                             <div className = {styles.AdvisoryHotline}>
                             Hotline: 0327846162
                             </div>
                         </div>
                     </div>
+                    </form>
                 </div>
                 <div className = {styles.AdvisoryMainImageGroup}>
                     <img className = {styles.AdvisoryMainImage} src="images/Bee_AdvImage.svg" alt="BigBee" />
