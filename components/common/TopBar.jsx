f4b3f59262d5f5e791634d23a4fe557e7d7b0a1e
RUKICUA <bxv.se.gd@gmail.com>
2021-06-10 22:51:04 +0700

    update match new design 10/6/2021

--- components/common/TopBar.jsx
+++ components/common/TopBar.jsx
@@ -12,6 +12,7 @@ function TopBar() {
     <div>
     <div className={styles.navbar} > 
       <img src="/images/logo.svg" alt="logo" className={styles.logo}/>
+      <img src="/images/logo.svg" alt="logo" className={styles.logoMobile}/>
       <div className={styles.navWrapper}>
         <div className={styles.navContent} >
           <a href="#Header"><img src="/images/mainpagebee.svg" alt="minibee" /><span className={styles.span}>Trang chủ</span></a>
@@ -19,9 +20,10 @@ function TopBar() {
           <a href="#Pricing">Gói dịch vụ</a>
           <a href="">Về chúng tôi</a>
         </div>
-        <button onClick={()=>setShowLinks(!showLinks)}><img src="/images/menuicon.png" alt="menu icon" /></button>
+        <button onClick={()=>setShowLinks(!showLinks)}><img src="/images/menuicon.svg" alt="menu icon" /></button>
+
       </div>
-      <img src="/images/logo.svg" alt="logo" className={styles.logoMobile}/>
+      
     </div>
     <div className={styles.navbarMobile}>
       <div className={styles.navWrapperMobile}>
