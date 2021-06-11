cc4dea4c17093d78609c1980a053fb1a450414cc
HHuy2000 <dyoujinh@gmail.com>
2021-06-11 20:16:35 +0700

    Changes

--- components/home/Testimonial.jsx
+++ components/home/Testimonial.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
-
+import AssCrack from "./testimonials.module.css";
 function Testimonial() {
     return (
         <div>
-            testimonial
+            <div className="AssCrack.testimonial-block">
+                <div className="AssCrack.userblock">
+                <div className="AssCrack.profile">
+                <img src="" alt="img1" className="AssCrack.usr-img"></img>
+                    <div className="AssCrack.usr-name">
+                    <h2>Trần Huy Quyền</h2>
+                    </div>
+                    <div className="AssCrack.usr-handle">
+                    <p>CLB Doanh nhân 2030</p>    
+                    </div>
+                </div>
+                <div className="AssCrack.comment">
+                    <p>Tôi luôn đánh giá cao chất lượng sản phẩm, dịch vụ của BeeX Nổi bật nhất là nhân viên tư vấn rất chuyên nghiệp và tận tâm. Chúc BeeX luôn thành công.</p>
+                </div>
+                </div>
+                <div className="AssCrack.userblock">
+                <div className="AssCrack.profile">
+                <img src="" alt="img2" className="AssCrack.usr-img"></img>
+                    <div className="AssCrack.usr-name">
+                    <h2>Lê Hồng Châu</h2>
+                    </div>
+                    <div className="AssCrack.usr-handle">
+                    <p>Công ty Cổ phần Nova F&B</p>    
+                    </div>
+                </div>
+                <div className="AssCrack.comment">
+                    <p>Các bạn nhân viên hỗ trợ rất nhiệt tình, xử lý thông tin và phản hồi rất nhanh. Mình khá là hài lòng với dịch vụ của bên BeeX. Hiện tại bên mình dùng rất nhiều dịch vụ tại BeeX.</p>
+                </div>
+                </div>
+                <div className="AssCrack.userblock">
+                <div className="AssCrack.profile">
+                <img src="" alt="img3" className="AssCrack.usr-img"></img>
+                    <div className="AssCrack.usr-name">
+                    <h2>Nguyễn Trung Thành</h2>
+                    </div>
+                    <div className="AssCrack.usr-handle">
+                    <p>Công ty TNHH TMDV Gia dụng Gold Fit</p>    
+                    </div>
+                </div>
+                <div className="AssCrack.comment">
+                    <p>Dịch vụ BeeX đã khiến cho doanh nghiệp GOLD FIT của tôi chuyên nghiệp hơn rất nhiều, sự hỗ trợ của BeeX là rất chuyên nghiệp, nhân viên lại dễ thương và đáng yêu. Tôi thích BeeX.</p>
+                </div>
+                </div>
+                <div className="AssCrack.next-btn">
+                    <img src="" alt="right arrow"></img>
+                </div>
+                <div className="AssCrack.previous-btn">
+                    <img src="" alt="left arrow"></img>
+                </div>
+            </div>
         </div>
     )
 }
